3b9b93c9c7422ccd0b02a632d690f3c50ad4f880
SylvianBurn <sylvian.burn@epitech.eu>
2021-12-08 13:23:47 +0100

    trying t do day 4

--- Day 3/part2.py
+++ Day 3/part2.py
@@ -1,60 +1,56 @@
 import sys
 
 def main():
-    File = open("Day 3\input", 'r')
+    File = open("Day 3\input2", 'r')
     Lines = File.readlines()
-    zero = []
-    one = []
-    bin = ""
-    bin2 = ""
-    newLines = []
+    Lines2 = Lines
 
-    for p in range(0, len(Lines[0]) - 1):
-        zero.append("")
-        one.append("")
-
-    for x in range(0, len(Lines) - 1):
-        for y in range(0, len(Lines[x]) - 1):
-            if int(Lines[x][y]) == 0:
-                zero[y] = zero[y] + ("0")
-            else:
-                one[y] = one[y] + ("1")
-
-            # print("zero = ",zero[index], " ", len(zero[index]), "one = ", one[index], " ", len(one[index]))
-    for index in range(0, len(Lines[0]) - 1):
+    for index in range(0, len(Lines[0])):
+        zero = 0
+        one = 0
         if len(Lines) == 1:
             break
-        print(index)
-        if len(zero[index]) < len(one[index]):
-            print("zero plus petit")
-            for t in range(0, len(Lines) - 1):
-                print("T = ", t," index = ", index, " ", Lines[t])
-                if int(Lines[t][index]) != 0:
-                    newLines.append(Lines[t])
-                    print("Newlines = ", newLines)
-            Lines = newLines
-            newLines.clear()
+        for item in Lines:
+            if item[index] == "0":
+                zero += 1
+            else:
+                one += 1
+        newLines = []
+        if zero <= one:
+            for line in range(0, len(Lines)):
+                if Lines[line][index] == "1":
+                    newLines.append(Lines[line])
         else:
-            print("one plus petit")
-            for m in range(0, len(Lines) - 1):
-                print("M = ", m," index = ", index, " ", Lines[t])
-                if int(Lines[t][index]) != 1:
-                    newLines.append(Lines[t])
-            Lines = newLines
-            newLines.clear()
-        print(Lines)
+            for line in range(0, len(Lines)):
+                if Lines[line][index] == "0":
+                    newLines.append(Lines[line])
+        Lines = newLines
 
-def pop_zero(Lines, index):
-    for i in range(0, len(Lines) - 1):
-        if Lines[i][index] == 0:
-            Lines.pop(i)
-    return Lines
+    for index in range(0, len(Lines2[0])):
+        zero = 0
+        one = 0
+        if len(Lines2) == 1:
+            break
+        for item in Lines2:
+            if item[index] == "0":
+                zero += 1
+            else:
+                one += 1
+        newLines2 = []
+        if zero <= one:
+            for line in range(0, len(Lines2)):
+                if Lines2[line][index] == "0":
+                    newLines2.append(Lines2[line])
+        else:
+            for line in range(0, len(Lines2)):
+                if Lines2[line][index] == "1":
+                    newLines2.append(Lines2[line])
+        Lines2 = newLines2
 
-def pop_one(Lines, index):
-    for i in range(0, len(Lines) - 1):
-        if Lines[i][index] == 1:
-            Lines.pop(i)
-    return Lines
+    oxy = int(Lines[0], 2)
+    oxy2 = int(Lines2[0], 2)
+    print(oxy * oxy2)
+
 
 if __name__ == "__main__":
     main()
